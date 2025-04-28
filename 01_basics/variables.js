76ea52ccfe38baec282eb3bd209b754aa65ad439
itshuzefa <iamhuzefashaikh@gmail.com>
2025-04-28 23:10:58 +0000

    variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -5,12 +5,37 @@
 //const: Cannot be re-decalred or updated.
 //Never use var. Only use let & const. 
 
-var name = "huzefa";
-// console.log(name)
-var name = "rehan";
-console.log(name)
+var name = "Rehan";
+//console.log(name); //Output: Rehan
 
-let fullName = "9";
-console.log(fullName)
-let fullName = "10";
-console.log(fullName)
+var name = "Fardeen";
+//console.log(name); //Output: Fardeen cz a new variable with same name was created.
+
+name = "Huzefa";
+// console.log(name); Output: New value was assigned successfully.
+
+// let rollNo = "20";
+// //console.log(rollNo)//output:20
+
+let rollNo = "10";
+//console.log(rollNo)//let cannot be re-decarled because it is already identifyed by the identifier
+
+rollNo = "5";
+console.log(rollNo);//value updated
+
+const darkMode = 'black';
+//console.log(darkMode);
+ 
+darkMode = 'white';
+console.log(darkMode);// value cannot be updated
+
+
+
+
+
+
+
+
+
+
+
