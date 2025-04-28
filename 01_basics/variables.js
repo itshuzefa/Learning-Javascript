42a6ca7045e675e3c0ef9fa2fd0b98011ba5798a
itshuzefa <iamhuzefashaikh@gmail.com>
2025-04-28 22:30:33 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+//First Chapter - Variables
+//var, let & const
+//var: Can be re-decraled and updated.
+//let: Cannot be re-declared but can be updated.
+//const: Cannot be re-decalred or updated.
+//Never use var. Only use let & const. 
+
+var name = "huzefa";
+// console.log(name)
+var name = "rehan";
+console.log(name)
+
+let fullName = "9";
+console.log(fullName)
+let fullName = "10";
+console.log(fullName)
