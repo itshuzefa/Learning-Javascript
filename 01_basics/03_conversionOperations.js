41aa00f4af750a334bce6bb2766c4a3153af0454
itshuzefa <iamhuzefashaikh@gmail.com>
2025-04-29 18:35:52 +0000

    conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -5,14 +5,31 @@ console.log(typeof name1);
 
 //Number to String
 let rollNo = 13;
-let rollNo1 = String (rollNo);
-console.log(rollNo1) 
+let rollNo2 = String (rollNo);;
+console.log(typeof rollNo2);
 
 //Number to Boolean
-//Boolean to Number
-//Boolean to String
-//Undefined to Null
-//Null to Number
-//Number to Null
-//String to Null
+let googleAccountId = 143;
+let googleAccountId2 = Boolean (googleAccountId);
+console.log(typeof googleAccountId2);
 
+//Boolean to Number
+let statment = true;
+let statment2 = Number (statment);
+console.log(typeof statment2);
+
+//Boolean to String
+let speech = false;
+let speech2 = String (speech);
+console.log(typeof speech2);
+
+/*Undefined to Null
+ let num = undefined;
+let num2 = null (num);
+console.log(typeof num2);*/
+
+//Null to Number
+//let num = null;
+//let num2 = number (num);
+//console.log(typeof num2);
+//String to Null
