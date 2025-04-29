9344df44ea678eb96d38e716a83e2811ac058ed3
itshuzefa <iamhuzefashaikh@gmail.com>
2025-04-29 10:45:36 +0000

    data type practice

--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -2,7 +2,25 @@
 Primitve: string, number, boolean, undefined, null, symbol, bigint
 Non-Primitive: object-array & functions*/
 
+//let name = "huzefa";
+//console.log(typeof name);
 
+//let num = 199;
+//console.log(typeof num);
 
+//let darkMode = true;
+//console.log(typeof darkMode);
+
+//let num = undefined;
+//console.log(typeof num);
+
+//let name = null;
+//console.log(typeof name);
+
+//let name = null;
+//console.log(typeof name);
+
+//let idName = Symbol;
+//console.log(typeof idName);
 
 
